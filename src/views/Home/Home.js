551d85cebf602c0f7808e6f055d68b88b898f49f
kunalsonwane77 <kunalsonwane77@gmail.com>
2024-09-12 19:53:16 +0530

    created basic structure

---
+++ src/views/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Home.css"
+import Card from '../../components/Fashioncard/Card'
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Fashionstor</h1>
+        <Card/>
+    </div>
+  )
+}
+
+export default Home
